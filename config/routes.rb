69e77d64166d35f6b386efed8592a61347628a41
Tran Ky Quang <tran.ky.quang@framgia.com>
2017-04-19 11:09:33 +0700

    create sign-up and edit profile page

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   root "static_pages#home"
   get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
+  get "/signup", to: "users#new"
+
+  resources :users
 end
