fefd2d8e331c4ed83aeeb974154695342097885a
Tran Ky Quang <tran.ky.quang@framgia.com>
2017-04-19 20:42:19 +0700

    complete login-logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,22 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by email: params[:sessions][:email].downcase
+    if user && user.authenticate(params[:sessions][:password])
+      log_in user
+      params[:sessions][:remember_me] == Settings.login_page.checked ?
+        remember(user): forget(user)
+      redirect_back_or user
+    else
+      flash.now[:danger] = t "login_page.invalid_password"
+      render :new
+    end
+  end
+
+  def destroy
+    log_out if logged_in?
+    redirect_to root_path
+  end
 end
